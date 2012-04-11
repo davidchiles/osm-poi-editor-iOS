55d7daafaa25021fc014b7189af882d0898eb63e
David Chiles <dwalterc@gmail.com>
2012-04-10 21:49:43 -0700

    minor bug fixes

---
+++ OSM POI Editor/OPECreditViewController.h
@@ -0,0 +1,13 @@
+//
+//  OPECreditViewController.h
+//  OSM POI Editor
+//
+//  Created by David Chiles on 4/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPECreditViewController : UIViewController
+
+@end
