32d68988a6e75cc1d0314287dd7889235ceb3991
David Chiles <dwalterc@gmail.com>
2012-04-12 10:52:59 -0700

    added zoom level message and warning

---
+++ OSM POI Editor/OPEMessage.h
@@ -0,0 +1,13 @@
+//
+//  OPEMessage.h
+//  OSM POI Editor
+//
+//  Created by David Chiles on 4/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPEMessage : UIView
+
+@end
---
+++ OSM POI Editor/OPEMessage.m
@@ -0,0 +1,31 @@
+//
+//  OPEMessage.m
+//  OSM POI Editor
+//
+//  Created by David Chiles on 4/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "OPEMessage.h"
+
+@implementation OPEMessage
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
