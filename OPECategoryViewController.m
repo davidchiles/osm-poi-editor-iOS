95b69fa1fcf0a9f1d7760202bc72fecbb791bbef
David Chiles <dwalterc@gmail.com>
2012-10-11 16:27:27 -0700

    added license

--- OPECategoryViewController.m
+++ OPECategoryViewController.m
@@ -3,8 +3,22 @@
 //  OSM POI Editor
 //
 //  Created by David Chiles on 2/13/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 David Chiles. All rights reserved.
 //
+//  This file is part of POI+.
+//
+//  POI+ is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  POI+ is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with POI+.  If not, see <http://www.gnu.org/licenses/>.
 
 #import "OPECategoryViewController.h"
 #import "OPETagInterpreter.h"
---
+++ readme.md
@@ -0,0 +1,10 @@
+# POI+
+#### iOS App
+
+##Thrid-party Libraries
+
+- [MBProgressHUD](https://github.com/jdg/MBProgressHUD)
+- [ASIHTTPRequest](http://allseeing-i.com/ASIHTTPRequest/)
+- [gtm-oauth2](http://code.google.com/p/gtm-oauth2/)
+- [TBXML](https://github.com/71squared/TBXML)
+- [route-me](https://github.com/route-me/route-me)
