ca1e5914718bbfcbf195ae6832ae953f70bd25af
David Chiles <dwalterc@gmail.com>
2013-03-14 16:23:13 -0700

    updated uploading and opening changesets

---
+++ OSM POI Editor/OPEChangeset.h
@@ -0,0 +1,13 @@
+//
+//  OPEChangeset.h
+//  OSM POI Editor
+//
+//  Created by David on 3/13/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OPEChangeset : NSObject
+
+@end
---
+++ OSM POI Editor/OPEChangeset.m
@@ -0,0 +1,13 @@
+//
+//  OPEChangeset.m
+//  OSM POI Editor
+//
+//  Created by David on 3/13/13.
+//
+//
+
+#import "OPEChangeset.h"
+
+@implementation OPEChangeset
+
+@end
