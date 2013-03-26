9e8aeecb1c772581841cd97cb80caed2ea76f804
David Chiles <dwalterc@gmail.com>
2013-03-26 16:55:03 -0700

    added displying rest of address fields

---
+++ OSM POI Editor/OPEPhoneEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  OPEPhoneEditViewController.h
+//  OSM POI Editor
+//
+//  Created by David on 3/26/13.
+//
+//
+
+#import "OPERecentlyUsedViewController.h"
+
+@interface OPEPhoneEditViewController : OPERecentlyUsedViewController
+
+@end
---
+++ OSM POI Editor/OPEPhoneEditViewController.m
@@ -0,0 +1,38 @@
+//
+//  OPEPhoneEditViewController.m
+//  OSM POI Editor
+//
+//  Created by David on 3/26/13.
+//
+//
+
+#import "OPEPhoneEditViewController.h"
+
+@interface OPEPhoneEditViewController ()
+
+@end
+
+@implementation OPEPhoneEditViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
