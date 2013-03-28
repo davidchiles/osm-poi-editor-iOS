88fb07e7a3cb9455e0969ed6ca62c9bc13cf005e
David Chiles <dwalterc@gmail.com>
2013-03-27 17:12:11 -0700

    Show edit name for no name streets

---
+++ OSM POI Editor/OPENameEditView.h
@@ -0,0 +1,13 @@
+//
+//  OPENameEditView.h
+//  OSM POI Editor
+//
+//  Created by David on 3/27/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OPENameEditView : UIView
+
+@end
---
+++ OSM POI Editor/OPENameEditView.m
@@ -0,0 +1,31 @@
+//
+//  OPENameEditView.m
+//  OSM POI Editor
+//
+//  Created by David on 3/27/13.
+//
+//
+
+#import "OPENameEditView.h"
+
+@implementation OPENameEditView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
